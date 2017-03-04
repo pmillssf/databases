9ab7b665ee47c865100a7cc49e0c7626d59c58ee
Hack Reactor Students <accounts@hackreactor.com>
2017-03-04 11:37:56 -0800

    Define database in schema.sql
    Clone chatter-box client from solution
    Add database scheme image

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,30 @@
 CREATE DATABASE chat;
 
 USE chat;
+CREATE TABLE usernames (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roomnames (
+  id INT NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  usernameId INT,
+  text VARCHAR(255) NOT NULL,
+  roomnameId INT,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_usernameId FOREIGN KEY (usernameId) REFERENCES usernames(id),
+  CONSTRAINT fk_roomnameId FOREIGN KEY (roomnameId) REFERENCES roomnames(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
 
 
 
